f4cb289dee63dbaa6840c74bbc70d810288b9685
YeDemirkiran <yedemirkiran27@gmail.com>
2024-04-05 17:21:02 +0300

    Add Eyelid control and animation

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -11,15 +11,27 @@ public class PlayerController : MonoBehaviour
 
     Vector3 playerEuler, cameraEuler;
 
+    [Header("Eyelids")]
+    [SerializeField] Animator eyelidsAnimator;
+    bool eyelidsShut = false;
+
     // Start is called before the first frame update
     void Start()
     {
         playerEuler = transform.localEulerAngles;
         cameraEuler = camera.localEulerAngles;
+
+        //Time.timeScale = 0.1f;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        MouseControl();
+        EyelidsControl();
+    }
+
+    void MouseControl()
     {
         playerEuler.y += Input.GetAxis("Mouse X") * mouseSensitivity.x * Time.deltaTime;
 
@@ -28,5 +40,26 @@ public class PlayerController : MonoBehaviour
 
         transform.localEulerAngles = playerEuler;
         camera.localEulerAngles = cameraEuler;
+    }
+
+    void EyelidsControl()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse2))
+        {
+            eyelidsShut = true;
+            eyelidsAnimator.SetBool("eyelidsShut", true);
+
+            Debug.Log("Eyelids shut!");
+        }
+        else if (Input.GetKeyUp(KeyCode.Mouse2))
+        {
+            eyelidsShut = false;
+
+            Debug.Log("Eyelids open!");
+            eyelidsAnimator.SetBool("eyelidsShut", false);
+        }
+
+        //eyelidsShut = Input.GetKey(KeyCode.Mouse2);
+        //eyelidsAnimator.SetBool("eyelidsShut", eyelidsShut);
     }
 }
