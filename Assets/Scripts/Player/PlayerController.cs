6ed21af75feeacdf3769c93f8b0b121c3a62c234
YeDemirkiran <yedemirkiran27@gmail.com>
2024-04-05 12:05:19 +0300

    Add PlayerController.cs

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Public settings, will be adjustable from the game settings
+    public Vector2 mouseSensitivity;
+    public Vector2 yClamp;
+    
+    [SerializeField] new Transform camera;
+
+    Vector3 playerEuler, cameraEuler;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerEuler = transform.localEulerAngles;
+        cameraEuler = camera.localEulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerEuler.y += Input.GetAxis("Mouse X") * mouseSensitivity.x * Time.deltaTime;
+
+        cameraEuler.x += Input.GetAxis("Mouse Y") * mouseSensitivity.y * Time.deltaTime;
+        cameraEuler.x = Mathf.Clamp(cameraEuler.x, yClamp.x, yClamp.y);
+
+        transform.localEulerAngles = playerEuler;
+        camera.localEulerAngles = cameraEuler;
+    }
+}
